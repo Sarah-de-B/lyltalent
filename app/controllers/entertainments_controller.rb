cf0b9f39fb4fe88461d03c42118f864d0b20a951
charles frechet <charles.frechet@gmail.com>
2025-08-26 16:21:19 +0200

    modif en cours, recuperation job samuel pour continuer

--- app/controllers/entertainments_controller.rb
+++ app/controllers/entertainments_controller.rb
@@ -1,2 +1,11 @@
 class EntertainmentsController < ApplicationController
+  before_action :set_entertainment, only: [:show]
+  def show
+  end
+
+  private
+
+  def set_entertainment
+    @entertainement = Entertainement.find(params[:id])
+  end
 end
