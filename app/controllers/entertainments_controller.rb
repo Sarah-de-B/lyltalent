269b24f0da5f3ffe9bd10c0ec5a70c8b6e166a2a
charles frechet <charles.frechet@gmail.com>
2025-08-27 17:33:17 +0200

    ajout de lappli en francais + modif des dates/heures

--- app/controllers/entertainments_controller.rb
+++ app/controllers/entertainments_controller.rb
@@ -1,24 +1,21 @@
-
-class EntertainmentApplicationsController < ApplicationController
-
 class EntertainmentsController < ApplicationController
   before_action :set_entertainment, only: [:show]
-  
+
   def index
     @entertainments = policy_scope(Entertainment)
     if params[:query].present?
       @entertainments = @entertainments.search_by_address(params[:query])
     end
   end
-  
+
   def show
     authorize @entertainment
   end
- 
+
   private
 
   def set_entertainment
     @entertainment = Entertainment.find(params[:id])
   end
-  
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def format_duration_minutes(minutes)
+    hours = minutes / 60
+    mins = minutes % 60
+    "#{hours}h#{sprintf('%02d', mins)}"
+  end
 end
