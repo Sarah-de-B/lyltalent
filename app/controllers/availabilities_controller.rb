ed426878465f621cddceab51a4ecde1be077cfca
exec zsh <jonathanfournier565@gmail.com>
2025-08-28 16:04:56 +0200

    en cours

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -1,2 +1,5 @@
 class AvailabilitiesController < ApplicationController
+  def index
+    @entertainments = current_user.entertainments.where(entertainment_applications: {status: "accepted"})
+  end
 end
