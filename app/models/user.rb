1c36f63043b61c1e7593845de629c3087046ccf7
sarah crosnier de bellaistre <sarah.debellaistre@audencia.com>
2025-08-28 17:49:47 +0200

    seeds changes, il manque encore le status

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,32 +17,33 @@ class User < ApplicationRecord
 
   # validations
   validates :artist_name, uniqueness: true, if: :musician?
+  validates :first_name, :last_name, :phone_number, :email, :role, presence: true
 
   validate :at_least_one_photo, if: :musician?
 
-  validates :role, inclusion: { in: ['musician', 'event_planner'] }
+  validates :role, inclusion: { in: ['artiste', "organisateur d'événements"] }
 
 
   # validations urls
-  validates :instagram_url, format: {
-    with: /\A(?:https?:)?\/\/(?:www\.)?(?:instagram\.com|instagr\.am)\/[A-Za-z0-9_\.]+\z/,
-    message: "must be a valid Instagram URL"
-  }, allow_blank: true
+  # validates :instagram_url, format: {
+  #   with: /\A(?:https?:)?\/\/(?:www\.)?(?:instagram\.com|instagr\.am)\/[A-Za-z0-9_\.]+\z/,
+  #   message: "must be a valid Instagram URL"
+  # }, allow_blank: true
 
-  validates :facebook_url, format: {
-    with: /\A(?:https?:)?\/\/(?:www\.)?(?:facebook|fb)\.com\/[A-Za-z0-9_\-\.]+\/?\z/,
-    message: "must be a valid Facebook URL"
-  }, allow_blank: true
+  # validates :facebook_url, format: {
+  #   with: /\A(?:https?:)?\/\/(?:www\.)?(?:facebook|fb)\.com\/[A-Za-z0-9_\-\.]+\/?\z/,
+  #   message: "must be a valid Facebook URL"
+  # }, allow_blank: true
 
-  validates :youtube_url, format: {
-    with: /\A(?:https?:)?\/\/(?:(?:www\.)?youtube\.com\/(?:watch\?v=|embed\/)|youtu\.be\/)[A-Za-z0-9\-_]+\z/,
-    message: "must be a valid YouTube URL"
-  }, allow_blank: true
+  # validates :youtube_url, format: {
+  #   with: /\A(?:https?:)?\/\/(?:(?:www\.)?youtube\.com\/(?:watch\?v=|embed\/)|youtu\.be\/)[A-Za-z0-9\-_]+\z/,
+  #   message: "must be a valid YouTube URL"
+  # }, allow_blank: true
 
-  validates :spotify_url, format: {
-    with: /\A(?:https?:\/\/open\.spotify\.com\/(track|user|artist|album)\/[a-zA-Z0-9]+(\/playlist\/[a-zA-Z0-9]+)?|spotify:(track|user|artist|album):[a-zA-Z0-9]+(:playlist:[a-zA-Z0-9]+)?)\z/,
-    message: "must be a valid Spotify URL"
-  }, allow_blank: true
+  # validates :spotify_url, format: {
+  #   with: /\A(?:https?:\/\/open\.spotify\.com\/(track|user|artist|album)\/[a-zA-Z0-9]+(\/playlist\/[a-zA-Z0-9]+)?|spotify:(track|user|artist|album):[a-zA-Z0-9]+(:playlist:[a-zA-Z0-9]+)?)\z/,
+  #   message: "must be a valid Spotify URL"
+  # }, allow_blank: true
 
   # ORGA DEVENT
   has_many :events, dependent: :destroy
@@ -51,9 +52,9 @@ class User < ApplicationRecord
 
   def musician?
 
-    role == 'musician'
+    role == 'artiste'
   end
-  # def musician?= role == 'musician'
+  # def musician?= role == 'artiste'
 
 
   def at_least_one_photo
