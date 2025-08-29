2b42b988b1d2b021ef26bfb7103049033497a143
Sarah-de-B <sarah.debellaistre@audencia.com>
2025-08-29 10:13:39 +0200

    Update chat.rb

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,6 +1,5 @@
 class Chat < ApplicationRecord
   belongs_to :user # celui qui a créé la candidature (artiste)
   belongs_to :entertainment_application
-
-  validates :message, presence: true
+  has_many :messages, dependent: :destroy
 end
