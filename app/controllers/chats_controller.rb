d8f02b4f36ba06940702a7d7bf86af784b6abb0a
Mailliw44 <william.magde@gmail.com>
2025-09-01 12:38:54 +0200

    page notif abandonné mais page et chat index faite

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,26 +1,20 @@
-
 class ChatsController < ApplicationController
   def index
-    # On récupère les event_planners liés aux applications acceptées
-    @chats = policy_scope(Chat.joins(:entertainment_application)
-                              .where(entertainment_application: {user: current_user}))
+    @chats = policy_scope(
+      Chat.joins(:entertainment_application)
+          .joins(:messages)
+          .where(entertainment_application: { user: current_user })
+          .group("chats.id")
+          # Trie les enregistrements (par exemple, les chats) en fonction de la date du dernier message, du plus récent au plus ancien
+          .order("MAX(messages.created_at) DESC")
+    )
 
     authorize @chats
   end
 
   def show
     @chat = Chat.find(params[:id])
+    @messages = @chat.messages.order(created_at: :asc)
     authorize @chat
-    @messages = @chat.messages
   end
-
-
-
-  #   def show
-  #   @chat = Chat.find(params[:id])
-  #   @new_chat = Chat.new
-  #   @message = Message.new
-  #   @objective = @chat.objective
-  #   @chats = @objective.chats
-  # end
 end
--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,8 @@
 class Chat < ApplicationRecord
   belongs_to :entertainment_application
   has_many :messages, dependent: :destroy
+
+  def last_message
+    messages.order(created_at: :desc).first
+  end
 end
