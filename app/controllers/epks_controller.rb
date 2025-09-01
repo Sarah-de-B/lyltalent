ffa486cf397d02e7ee510941acef7ccdc28df4fb
samuel dorven <sdorven@outlook.fr>
2025-09-01 16:39:19 +0200

    Mise en place de l'édit pour l'EPK

--- app/controllers/epks_controller.rb
+++ app/controllers/epks_controller.rb
@@ -12,8 +12,8 @@ class EpksController < ApplicationController
 
   def update
     authorize @epk
-    if @epk.update(epk_params)
-      redirect_to edit_epk_path, notice: "EPK mis à jour avec succès"
+    if @epk.update(user_params)
+      redirect_to epk_path, notice: "EPK mis à jour avec succès !"
     else
       render :edit
     end
@@ -23,6 +23,17 @@ class EpksController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
   def set_epk
     @epk = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :artist_name, :artist_type, :bio, :original_composition, :covers,
+      :description_experience, :concert_number, :description_education,
+      :tools, :tools_needed, :vehicule, :legal_entity_type, :siret,
+      :phone_number, :instagram_url, :facebook_url, :spotify_url, :youtube_url, :address,
+      instruments: [], musical_styles: [],
+      videos: [], photos: []
+    )
   end
 
 end
