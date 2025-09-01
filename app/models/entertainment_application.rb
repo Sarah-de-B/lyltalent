f24cea2866779921b39eaed8661b0a1dbfb36843
exec zsh <jonathanfournier565@gmail.com>
2025-09-01 17:09:33 +0200

    validation

--- app/models/entertainment_application.rb
+++ app/models/entertainment_application.rb
@@ -3,7 +3,7 @@ class EntertainmentApplication < ApplicationRecord
   belongs_to :entertainment
   has_one :chat, dependent: :destroy
   validates :message, presence: true
-
+  validates :user_id, uniqueness: { scope: :entertainment_id }
 
   validates :status, inclusion: { in: ["En cours", "Accepté", "Refusé", "Proposé", "Validé"] }
 
