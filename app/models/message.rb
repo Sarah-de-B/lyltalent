a889fcb2921f516e9e1c07a40ea11dc55213e2e3
exec zsh <jonathanfournier565@gmail.com>
2025-09-02 17:41:24 +0200

    refresh en cours

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,14 @@ class Message < ApplicationRecord
   belongs_to :chat
   belongs_to :user
   validates :content, presence: true
+  after_create_commit :broadcast_message
+
+private
+
+  def broadcast_message
+    broadcast_append_to "chat_#{chat.id}_messages",
+                        partial: "messages/message",
+                        target: "messages",
+                        locals: { message: self }
+  end
 end
