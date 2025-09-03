299feb0f3d523a1ff476b411b7030552973a6920
exec zsh <jonathanfournier565@gmail.com>
2025-09-03 10:45:27 +0200

    refresh

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,12 +4,17 @@ class Message < ApplicationRecord
   validates :content, presence: true
   after_create_commit :broadcast_message
 
+  def sender?(a_user)
+    user.id == a_user.id
+  end
+
 private
 
   def broadcast_message
     broadcast_append_to "chat_#{chat.id}_messages",
                         partial: "messages/message",
                         target: "messages",
-                        locals: { message: self }
+                        locals: { message: self, user: user }
   end
+
 end
